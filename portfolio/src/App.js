a05743b0771dc293ccc3f4f78eabd94f6547f96b
phantomramen <ali.hernandez9two@gmail.com>
2021-01-24 18:14:09 -0700

    added reactstrap navbar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import NaviBar from './components/Navbar/Navbar'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col md={7}>
+          <h1>Alí Hernandez</h1>
+        </Col>
+        <Col md ={5}>
+          <h2>Search Bar</h2>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={12}>
+          <NaviBar />
+        </Col>
+      </Row>
+      <Row>
+        <Col md={12}>
+          <h2>About Section Here</h2>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
