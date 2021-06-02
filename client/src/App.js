8db7139b5146a0b8b75b5929a4a018f88962c5b6
phantomramen <ali.hernandez9two@gmail.com>
2021-06-01 21:51:41 -0600

    basic html

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css'; 
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <div className="App">
+     <Contact />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Contact.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './Contact.js';
+
+const Contact = () => {
+    return (
+        <div className="main-container">
+            
+            <div className="central-form">
+                <h2 className="title">Contact form</h2>
+
+                <div className="back-form">
+                    <div className="img-back">
+                        <h4>Send your message</h4>
+                        <img src="" alt="" />
+                    </div>
+
+                    <form>
+                        <p>message from server</p>
+
+                        <label htmlFor="name">Name</label>
+                        <input type="text" placeholder="your name here..." />
+
+                        <label htmlFor="email">Email</label>
+                        <input type="email" placeholder="your email here..." />
+
+                        <label htmlFor="message">Message</label>
+                        <textarea type="text" placeholder="your message here..." name="message"/>
+
+                        <div className="send-btn">
+                            <button type="submit">Send</button>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Contact;
